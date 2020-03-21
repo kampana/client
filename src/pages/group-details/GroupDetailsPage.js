0cb6f4dcb53a2792388dd7e96a15382b1d41dac4
omer <omerz3358@gmail.com>
2020-03-22 01:19:26 +0200

    added group card with hardcoded data as per schema to GroupDetailsPage

--- src/pages/group-details/GroupDetailsPage.js
+++ src/pages/group-details/GroupDetailsPage.js
@@ -1,6 +1,21 @@
 // @flow
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Card, Icon } from 'semantic-ui-react';
+
+const HARDCODED_GROUP = {
+  name: 'Code for Australia',
+  countryCode: 'AU',
+  description: 'Help for Australia',
+  links:
+    'Our awesome Slack channel>>>https://foo.slack.com>>>SLACK|||Our sweet Twitter account>>>https://twitter.com/codeforaustralia>>>TWITTER|||Our amazing website>>>https://www.codeforaustralie.com>>>WEBSITE',
+};
+
+const LINK_TYPE_ICONS = {
+  // TODO: add all possible link type icons
+  SLACK: 'slack',
+  TWITTER: 'twitter',
+  WEBSITE: 'linkify',
+};
 
 const GroupDetailsPage = props => {
   const {
@@ -9,7 +24,29 @@ const GroupDetailsPage = props => {
 
   console.log('group id: ', params.groupId);
 
-  return <Container>Here goes the group details content</Container>;
+  // TODO: maybe move this logic into a new separate GroupLinks component?
+  const groupLinks = HARDCODED_GROUP.links.split('|||');
+  const extra = groupLinks.map(groupLink => {
+    const [linkDescription, linkUrl, linkType] = groupLink.split('>>>');
+    return (
+      <div>
+        {linkDescription} - <a href={linkUrl}>{linkUrl}</a>
+        <br />
+        <Icon name={LINK_TYPE_ICONS[linkType]} />
+      </div>
+    );
+  });
+
+  return (
+    <Container>
+      <Card
+        header={HARDCODED_GROUP.name}
+        meta={HARDCODED_GROUP.countryCode}
+        description={HARDCODED_GROUP.description}
+        extra={extra}
+      />
+    </Container>
+  );
 };
 
 export default GroupDetailsPage;
