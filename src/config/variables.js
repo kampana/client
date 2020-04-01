f3579f5da86202570bbd9ee050ee2adbcedf4419
Konstantin Werner <konstantin@webforte.io>
2020-04-01 23:22:45 +0200

    reference "blue" als primary color

--- src/config/variables.js
+++ src/config/variables.js
@@ -5,12 +5,13 @@
 export const white = '#ffffff';
 export const black = '#000000';
 
+export const blue = '#061f71';
 export const beige = '#ffe1b2';
 export const grey = '#6e6e6e';
 export const greyDark = '#2c2c2c';
 export const blueLight = '#f5f8fa';
 
-export const primary = '#061f71';
+export const primary = blue;
 export const secondaryColor = greyDark;
 export const ternaryColor = grey;
 
